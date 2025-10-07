fd245270aac728810e3e0587a7c8a7fae705308c
Saadat Ali <saadatali0202@gmail.com>
2025-10-07 18:40:28 +0500

    chore(api): import and register necessary modules in AppModule

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -3,9 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaService } from './common/prisma.service';
+import { StoreModule } from './store/store.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [ConfigModule.forRoot(), StoreModule, AuthModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
